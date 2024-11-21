818b936ca1e2a0dc41ff91d082bf90d680df32c4
KattyTotoro <9184036854@yandex.ru>
2024-11-21 20:50:14 +0300

    jhgjhj

--- pages/podcast.vue
+++ pages/podcast.vue
@@ -3,7 +3,52 @@
         <h1>Подкаст</h1>
 
         <div>
-            <h2>Запись 1</h2>
+            <h2>Запись 8</h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
+            reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
+            quaerat sunt nam non dolores blanditiis.
+            <br>
+            <a href="#">Подробнее</a>
+        </div>
+
+        <div>
+            <h2>Запись 7</h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
+            reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
+            quaerat sunt nam non dolores blanditiis.
+            <br>
+            <a href="#">Подробнее</a>
+        </div>
+
+        <div>
+            <h2>Запись 6</h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
+            reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
+            quaerat sunt nam non dolores blanditiis.
+            <br>
+            <a href="#">Подробнее</a>
+        </div>
+
+        <div>
+            <h2>Запись 5</h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
+            reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
+            quaerat sunt nam non dolores blanditiis.
+            <br>
+            <a href="#">Подробнее</a>
+        </div>
+
+        <div>
+            <h2>Запись 4</h2>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
+            reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
+            quaerat sunt nam non dolores blanditiis.
+            <br>
+            <a href="#">Подробнее</a>
+        </div>
+
+        <div>
+            <h2>Запись 3</h2>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
             reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
             quaerat sunt nam non dolores blanditiis.
@@ -16,48 +61,17 @@
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
             reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
             quaerat sunt nam non dolores blanditiis.
+            <br>
+            <a href="#">Подробнее</a>
         </div>
 
         <div>
-            <h2>Запись 3</h2>
+            <h2>Запись 1</h2>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
             reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
             quaerat sunt nam non dolores blanditiis.
-        </div>
-
-        <div>
-            <h2>Запись 4</h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
-            reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
-            quaerat sunt nam non dolores blanditiis.
-        </div>
-
-        <div>
-            <h2>Запись 5</h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
-            reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
-            quaerat sunt nam non dolores blanditiis.
-        </div>
-
-        <div>
-            <h2>Запись 6</h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
-            reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
-            quaerat sunt nam non dolores blanditiis.
-        </div>
-
-        <div>
-            <h2>Запись 7</h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
-            reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
-            quaerat sunt nam non dolores blanditiis.
-        </div>
-
-        <div>
-            <h2>Запись 8</h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
-            reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
-            quaerat sunt nam non dolores blanditiis.
+            <br>
+            <a href="#">Подробнее</a>
         </div>
 
     </section>
